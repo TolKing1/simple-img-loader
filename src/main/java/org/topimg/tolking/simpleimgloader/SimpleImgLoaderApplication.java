c959c77a7b53a0dcc96411479a5524514ae45edf
To'lqin Oltiboyev <tolking0101@gmail.com>
2024-04-16 18:04:49 +0500

    INITIAL commit

---
+++ src/main/java/org/topimg/tolking/simpleimgloader/SimpleImgLoaderApplication.java
@@ -0,0 +1,13 @@
+package org.topimg.tolking.simpleimgloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleImgLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleImgLoaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/topimg/tolking/simpleimgloader/SimpleImgLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.topimg.tolking.simpleimgloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleImgLoaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
